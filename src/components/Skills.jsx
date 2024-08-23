294382d73d53a433272c8477fe7a1fdd657150b5
INTSEAN <119046561+INTSEAN@users.noreply.github.com>
2024-08-22 21:53:48 -0400

    Updated Skills Sections

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,9 +2,16 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-import meterOne from "../assets/img/meter1.svg";
-import meterTwo from "../assets/img/meter2.svg";
-import meterThree from "../assets/img/meter3.svg";
+import langOne from "../assets/img/lang1.svg";
+import langTwo from "../assets/img/lang2.svg";
+import langThree from "../assets/img/lang3.svg";
+import langFour from "../assets/img/lang4.svg";
+import langFive from "../assets/img/lang5.svg";
+import langSeven from "../assets/img/lang7.svg";
+import langEight from "../assets/img/lang8.svg";
+import langNine from "../assets/img/lang9.svg";
+import langEleven from "../assets/img/lang11.svg";
+
 import colorSharp from "../assets/img/color-sharp.png";
 
 function Skills() {
@@ -35,28 +42,39 @@ function Skills() {
           <Row>
             <Col>
               <div className="skills-bx">
-                <h2>Skills</h2>
-                <p> What can I do? Look at the section below </p>
+                <h2>Programming Languages + Frameworks</h2>
+                <p> I speak fluent... </p>
                 <Carousel
                   responsive={responsive}
                   infinite={true}
                   className="skill-slider"
                 >
                   <div className="item">
-                    <img src={meterThree} alt="Image" />
-                    <h5>Machine Learning Development</h5>
+                    <img src={langOne} alt="Image" />
                   </div>
                   <div className="item">
-                    <img src={meterOne} alt="Image" />
-                    <h5>Blockchain Development</h5>
+                    <img src={langTwo} alt="Image" />
                   </div>
                   <div className="item">
-                    <img src={meterOne} alt="Image" />
-                    <h5>Web Development</h5>
+                    <img src={langThree} alt="Image" />
                   </div>
                   <div className="item">
-                    <img src={meterOne} alt="Image" />
-                    <h5>Artificial Intelligence Development</h5>
+                    <img src={langFour} alt="Image" />
+                  </div>
+                  <div className="item">
+                    <img src={langFive} alt="Image" />
+                  </div>
+                  <div className="item">
+                    <img src={langSeven} alt="Image" />
+                  </div>
+                  <div className="item">
+                    <img src={langEight} alt="Image" />
+                  </div>
+                  <div className="item">
+                    <img src={langNine} alt="Image" />
+                  </div>
+                  <div className="item">
+                    <img src={langEleven} alt="Image" />
                   </div>
                 </Carousel>
               </div>
